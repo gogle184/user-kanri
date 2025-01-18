158c3879611560d3e5764e38cb6b3296d9e78ad7
gogle184 <gogle184@gmail.com>
2025-01-19 02:21:16 +0900

    fix:ダミーユーザーデータ取得

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,16 +1,29 @@
-import { type FC, memo } from "react";
-import { HStack, Flex} from "@chakra-ui/react";
+import { type FC, memo, useEffect } from "react";
+import { HStack, Flex, Spinner, Center } from "@chakra-ui/react";
 import { UserCard } from "@/components/organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: FC = memo(() => {
+	const { getAllUsers, loading, users } = useAllUsers();
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		getAllUsers();
+	}, []);
+
 	return (
-		<HStack wrap="wrap" p={{ base: 4, md: 10 }}>
-				<Flex>
+    <>
+    {loading ? <Center h="100vh"><Spinner /></Center> : (
+      <HStack wrap="wrap" p={{ base: 4, md: 10 }} >
+          {users.map((user)=>(
+          <Flex key={user.id} mx="auto">
           <UserCard
           imageUrl="https://picsum.photos/160"
-          userName="げんげん"
-          fullName="佐藤" />
-        </Flex>
-		</HStack>
+          userName={user.username}
+          fullName={user.name} />
+          </Flex>
+          ))}
+      </HStack>
+    )}
+    </>
 	);
 });
---
+++ src/hooks/useAllUsers.ts
@@ -0,0 +1,25 @@
+import { useState, useCallback } from "react";
+import axios from "axios";
+import type { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
+
+export const useAllUsers = () => {
+  const { showMessage } = useMessage();
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState<Array<User>>([]);
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  const getAllUsers = useCallback(() => {
+    setLoading(true);
+    axios
+    .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+    .then((res) => setUsers(res.data))
+    .catch(() => {
+      showMessage({ title: "ユーザー取得に失敗しました", type: "error" });
+    })
+    .finally(() => {
+      setLoading(false);
+    });
+  }, []);
+
+  return { getAllUsers, loading, users };
+};
